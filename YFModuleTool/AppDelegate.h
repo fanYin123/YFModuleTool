1f3f90bc9d4305eca1dc5bdf92da455bdd98df66
test <test@testdeiMac-2.local>
2021-04-20 09:01:12 +0800

    init project

---
+++ YFModuleTool/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  YFModuleTool
+//
+//  Created by test on 2021/4/19.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
